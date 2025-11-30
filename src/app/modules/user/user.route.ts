060460816bfcd9e6ad5a4d85a430a6eef2adc905
istiak19 <istiak15-14128@diu.edu.bd>
2025-11-30 20:43:57 +0600

    cloudinary setup

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userController } from "./user.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "@prisma/client";
+import { multerUpload } from "../../../config/multer.config";
 
 const router = Router();
 
 router.post("/register", userController.createUser);
 router.get("/", checkAuth(Role.ADMIN), userController.getAllUsers);
 router.get("/:id", checkAuth(Role.ADMIN, Role.GUIDE, Role.TOURIST), userController.getSingleUser);
+router.patch("/:id", checkAuth(Role.ADMIN, Role.GUIDE, Role.TOURIST), multerUpload.single("file"), userController.getSingleUser);
 
 export const userRoutes = router;
